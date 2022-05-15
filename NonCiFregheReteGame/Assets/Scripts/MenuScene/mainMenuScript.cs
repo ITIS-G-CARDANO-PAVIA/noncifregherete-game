095dce4f64371ee3286c72088061e19ccf950347
Matteo Vacalebri <thet3o@yahoo.com>
2022-05-15 13:47:59 +0200

    game update

--- NonCiFregheReteGame/Assets/Scripts/MenuScene/mainMenuScript.cs
+++ NonCiFregheReteGame/Assets/Scripts/MenuScene/mainMenuScript.cs
@@ -7,18 +7,20 @@ using UnityEngine.SceneManagement;
 public class mainMenuScript : MonoBehaviour
 {
     //Finale 110, 111, 112
+    public static string ipAddress = "0.0.0.0";
 
-    [SerializeField]
-    private InputField inp;
-    public static string ipAddress;
-
-    public void playGame() { 
-        ipAddress = inp.ToString();
+    public void playGame() {
         Network net = new Network(ipAddress);
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+        SceneManager.LoadScene(1); 
     }
-    
+
+    public void setRobotIp(string s)
+    {
+        ipAddress = s;
+        Debug.Log(ipAddress.ToString());
+    }
+
     public void quitGame(){
         Application.Quit();
         Debug.Log("QUIT!");
